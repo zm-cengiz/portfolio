a8d2491c0c21ba7fdd48399e4967b0127ad7089d
zehra-cengiz_deopca <zehra.cengiz@torontomu.ca>
2026-04-11 01:25:54 -0400

    CI/CD pipeline check

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,7 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm a Computer Science student that is graduating this summer. Wish me luck :) `}
       </p>
       <div className="my-8">
         <BlogPosts />
